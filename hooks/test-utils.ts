f2c2874675428231fd68e1b96ddcda5641c6391c
Matthew Herod <matthew.herod@gmail.com>
2026-03-14 17:11:33 +0000

    fix(test-utils): use Promise.all for stdout/stderr drain

--- hooks/test-utils.ts
+++ hooks/test-utils.ts
@@ -65,8 +65,10 @@ export async function runHook(
   void proc.stdin.write(payload)
   void proc.stdin.end()
 
-  const stdout = await new Response(proc.stdout).text()
-  const stderr = await new Response(proc.stderr).text()
+  const [stdout, stderr] = await Promise.all([
+    new Response(proc.stdout).text(),
+    new Response(proc.stderr).text(),
+  ])
   await proc.exited
 
   let decision: string | undefined
